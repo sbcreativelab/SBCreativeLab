2e3438d0ae26783811ae15e366ba5ce35e3e97df
Ian Wen <ianwen@Ians-Pro.local>
2022-12-26 19:26:54 -0800

    Initial commit for Winter 2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Route } from "react-router-dom";
-import './main.css'
+// import './main.css'
+import './w23proj.css'
 import Home from './Components/Home'
 import Menu from './Components/Menu'
 import Oops from './Components/Oops'
@@ -11,6 +12,7 @@ import ReactGA from 'react-ga'
 import Challenge from './Components/Challenge';
 import Designathon from './Components/Designathon';
 import Projects from './Components/Projects';
+import W23Projects from './Components/W23Projects';
 
 export class App extends Component {
   constructor(props) {
@@ -38,6 +40,7 @@ export class App extends Component {
         </head>
         <div className="main-body">
           <Route exact path="/" render={(props) => <Projects {...props} />} />
+          <Route exact path="/w23" render={(props) => <W23Projects {...props} />} />
           {/* <Route exact path="/challenge" render={(props) => <Challenge {...props}/>}/>
               <Route exact path="/oops" render={(props) => <Oops {...props}/>}/>
               <Route exact path="/about" render={(props) => <About {...props}/>}/>
---
+++ src/Components/W23Projects.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import ReactGA from 'react-ga'
+import { ReactComponent as Logo } from "./logo.svg";
+import useWindowDimensions from '../utils/useWindowDimensions';
+import { HashLink } from 'react-router-hash-link';
+
+const scrollTrigger = window.ScrollTrigger
+const gsap = window.gsap;
+
+gsap.registerPlugin(scrollTrigger)
+
+const W23Projects = () => {
+    ReactGA.initialize('UA-178117149-1');
+    ReactGA.pageview('projects');
+    const applicationLink = "https://forms.gle/6zuYsBn22MkPw1W67"
+    const currentQuarter = "Fall 2022"
+    return (
+        <div>
+            <div className="title-section">
+                <div className="logo-wrapper">
+                    <a href="https://www.sbcreativelab.com">
+                        <Logo id="logo"/>
+                    </a>
+                </div>
+                <div className="tab-list">
+                    <a href={applicationLink} target="_blank" className='tab'>Apply</a>
+                    <HashLink smooth to={`#about`} className='tab'>About</HashLink>
+                    <HashLink smooth to={`#faq`} className='tab'>FAQ</HashLink>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Projects;
