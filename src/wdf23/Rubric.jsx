2f7875daa6530b93e76b15973a88b8c7933a42e6
jacksonjude <jacksonjude@mac.com>
2023-01-09 01:04:12 -0800

    Spaced rubric code

--- src/wdf23/Rubric.jsx
+++ src/wdf23/Rubric.jsx
@@ -13,19 +13,47 @@ export default function Rubric() {
                 <img className='rubric-project-title-arrow' src={rubricArrow} alt='' />
             </div>
             <div className='rubric-container rubric-album-cover-container'>
-                <RubricSection maxWidth={420} title={"Composition & Design"} description={"Strong and unique composition. Layout is cohesive and actively contributes to the meaning of the design."} />
-                <RubricSection maxWidth={420} title={"Originality & Creativity"} description={"Excellent approach to developing and clearly expressing an original idea, with evidence of risk-taking."} />
-                <RubricSection maxWidth={420} title={"Rendering & Completion"} description={"No noticeable gaps or discrepancies within the design. Design is mostly to fully done with a clear concept."} />
+                <RubricSection
+                    maxWidth={420}
+                    title={"Composition & Design"}
+                    description={"Strong and unique composition. Layout is cohesive and actively contributes to the meaning of the design."}
+                />
+                <RubricSection
+                    maxWidth={420}
+                    title={"Originality & Creativity"}
+                    description={"Excellent approach to developing and clearly expressing an original idea, with evidence of risk-taking."}
+                />
+                <RubricSection
+                    maxWidth={420}
+                    title={"Rendering & Completion"}
+                    description={"No noticeable gaps or discrepancies within the design. Design is mostly to fully done with a clear concept."}
+                />
             </div>
             <div className='rubric-project-title rubric-music-fest-title'>
                 <div className='rubric-project-title-text'>Music Festival Lineup Poster.</div>
                 <img className='rubric-project-title-arrow' src={rubricArrow} alt='' />
             </div>
             <div className='rubric-container rubric-music-fest-container'>
-                <RubricSection maxWidth={320} title={"Composition & Design"} description={"Strong and unique composition. Layout is cohesive and actively contributes to the meaning of the design."} />
-                <RubricSection maxWidth={320} title={"Originality & Creativity"} description={"Excellent approach to developing and clearly expressing an original idea, with evidence of risk-taking."} />
-                <RubricSection maxWidth={320} title={"Information Hierarchy"} description={"Presents different types of text in an intuitive manner that does not overwhelm the reader."} />
-                <RubricSection maxWidth={320} title={"Use of Copy & Legibility"} description={"Utilizes copy efficiently and creatively, without compromising the legibility of the text."} />
+                <RubricSection
+                    maxWidth={320}
+                    title={"Composition & Design"}
+                    description={"Strong and unique composition. Layout is cohesive and actively contributes to the meaning of the design."}
+                />
+                <RubricSection
+                    maxWidth={320}
+                    title={"Originality & Creativity"}
+                    description={"Excellent approach to developing and clearly expressing an original idea, with evidence of risk-taking."}
+                />
+                <RubricSection
+                    maxWidth={320}
+                    title={"Information Hierarchy"}
+                    description={"Presents different types of text in an intuitive manner that does not overwhelm the reader."}
+                />
+                <RubricSection
+                    maxWidth={320}
+                    title={"Use of Copy & Legibility"}
+                    description={"Utilizes copy efficiently and creatively, without compromising the legibility of the text."}
+                />
             </div>
     	</div>
     )
