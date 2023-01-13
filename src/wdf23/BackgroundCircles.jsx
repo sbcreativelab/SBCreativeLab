74c72ae3ba1d15ebb9ff1c97829b6f9460342539
jacksonjude <jacksonjude@mac.com>
2023-01-12 16:31:05 -0800

    Added mobile background circles

--- src/wdf23/BackgroundCircles.jsx
+++ src/wdf23/BackgroundCircles.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 import './BackgroundCircles.css'
 
-import backgroundCircles1 from '../wdf23/imgs/background-circles-1.svg'
-import backgroundCircles2 from '../wdf23/imgs/background-circles-2.svg'
-import backgroundCircles3 from '../wdf23/imgs/background-circles-3.svg'
-import backgroundCircles4 from '../wdf23/imgs/background-circles-4.svg'
-import backgroundCircles5 from '../wdf23/imgs/background-circles-5.svg'
-import backgroundCircles6 from '../wdf23/imgs/background-circles-6.svg'
-import backgroundCircles7 from '../wdf23/imgs/background-circles-7.svg'
-import backgroundCircles8 from '../wdf23/imgs/background-circles-8.svg'
-import backgroundCircles9 from '../wdf23/imgs/background-circles-9.svg'
+import backgroundCircles1 from './imgs/background-circles-1.svg'
+import backgroundCircles1Small from './imgs/background-circles-1-small.svg'
+import backgroundCircles2 from './imgs/background-circles-2.svg'
+import backgroundCircles2Small from './imgs/background-circles-2-small.svg'
+import backgroundCircles3 from './imgs/background-circles-3.svg'
+import backgroundCircles3Small from './imgs/background-circles-3-small.svg'
+import backgroundCircles4 from './imgs/background-circles-4.svg'
+import backgroundCircles4Small from './imgs/background-circles-4-small.svg'
+import backgroundCircles5 from './imgs/background-circles-5.svg'
+import backgroundCircles5Small from './imgs/background-circles-5-small.svg'
+import backgroundCircles6 from './imgs/background-circles-6.svg'
+import backgroundCircles6Small from './imgs/background-circles-6-small.svg'
+import backgroundCircles7 from './imgs/background-circles-7.svg'
+import backgroundCircles7Small from './imgs/background-circles-7-small.svg'
+import backgroundCircles8 from './imgs/background-circles-8.svg'
+import backgroundCircles8Small from './imgs/background-circles-8-small.svg'
+import backgroundCircles9 from './imgs/background-circles-9.svg'
+import backgroundCircles9Small from './imgs/background-circles-9-small.svg'
 
 export default function BackgroundCircles() {
+    const isMobile = useMediaQuery({ query: `(max-width: 1300px)` });
+
     return (
     	<div className='background-circles'>
-    		<img className='background-circles-1' src={backgroundCircles1} alt='' />
-            <img className='background-circles-2' src={backgroundCircles2} alt='' />
-            <img className='background-circles-3' src={backgroundCircles3} alt='' />
-            <img className='background-circles-4' src={backgroundCircles4} alt='' />
-            <img className='background-circles-5' src={backgroundCircles5} alt='' />
-            <img className='background-circles-6' src={backgroundCircles6} alt='' />
-            <img className='background-circles-7' src={backgroundCircles7} alt='' />
-            <img className='background-circles-8' src={backgroundCircles8} alt='' />
-            <img className='background-circles-9' src={backgroundCircles9} alt='' />
+    		<img className='background-circles-1' src={!isMobile ? backgroundCircles1 : backgroundCircles1Small} alt='' />
+            <img className='background-circles-2' src={!isMobile ? backgroundCircles2 : backgroundCircles2Small} alt='' />
+            <img className='background-circles-3' src={!isMobile ? backgroundCircles3 : backgroundCircles3Small} alt='' />
+            <img className='background-circles-4' src={!isMobile ? backgroundCircles4 : backgroundCircles4Small} alt='' />
+            <img className='background-circles-5' src={!isMobile ? backgroundCircles5 : backgroundCircles5Small} alt='' />
+            <img className='background-circles-6' src={!isMobile ? backgroundCircles6 : backgroundCircles6Small} alt='' />
+            <img className='background-circles-7' src={!isMobile ? backgroundCircles7 : backgroundCircles7Small} alt='' />
+            <img className='background-circles-8' src={!isMobile ? backgroundCircles8 : backgroundCircles8Small} alt='' />
+            <img className='background-circles-9' src={!isMobile ? backgroundCircles9 : backgroundCircles9Small} alt='' />
     	</div>
     )
 }
