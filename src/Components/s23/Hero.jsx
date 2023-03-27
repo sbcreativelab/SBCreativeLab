3962156aaa9f799c184d19ef731d1951189ba388
Ian Wen <h_wen@ucsb.edu>
2023-03-27 02:45:59 -0700

    added phone view for hero

--- src/Components/s23/Hero.jsx
+++ src/Components/s23/Hero.jsx
@@ -1,49 +1,59 @@
 import React from 'react'
 import './Hero.css'
 
-import logo from '../s23_images/logo.svg'
-import title from '../s23_images/hero-title.svg'
-import descriptionDeco from '../s23_images/hero-description-deco.svg'
-import blob1 from '../s23_images/blob-1.svg'
+import logo from '../../images/s23_images/logo.svg'
+import titleLarge from '../../images/s23_images/hero-title.svg'
+import blob1 from '../../images/s23_images/blob-1.svg'
+import titleSmallSBCL from '../../images/s23_images/phone_view/hero-sbcl.svg'
+import titleSmallJumpstart from '../../images/s23_images/phone_view/hero-jumpstart.svg'
+import titleSmallDesignathon from '../../images/s23_images/phone_view/hero-spring_designathon.svg'
 
 export default function Hero() {
-    var applicationLink = "https://tinyurl.com/SBCLDesignathon23"
+    const applicationLink = 'https://tinyurl.com/SBCLDesignathon23'
     return (
         <div className='hero'>
-            <img src={logo} className='logo' alt='' />
-            <div className='navbar'>
-                <NavBarItem name={"Prompt"} />
-                <NavBarItem name={"Timeline"} />
-                <NavBarItem name={"Judges"} />
-                <NavBarItem name={"Mentors"} />
-                <NavBarItem name={"Prizes"} />
-                <NavBarItem name={"FAQ"} />
-                <NavBarItem name={"Sponsors"} />
-            </div>
+            <header>
+                <img src={logo} className='logo' alt='' />
+                <nav style={{ display: 'none' }}>
+                    <NavBarItem name={"Prompt"} />
+                    <NavBarItem name={"Timeline"} />
+                    <NavBarItem name={"Judges"} />
+                    <NavBarItem name={"Mentors"} />
+                    <NavBarItem name={"Prizes"} />
+                    <NavBarItem name={"FAQ"} />
+                    <NavBarItem name={"Sponsors"} />
+                </nav>
+            </header>
             <div className='hero-content'>
-                <img src={title} className='hero-title' alt='' />
+                <div className='hero-title'>
+                    <img className='hero-title-large-view' src={titleLarge} alt='' />
+                    <div className='hero-title-small-view'>
+                        <img src={titleSmallSBCL} alt='' />
+                        <img src={titleSmallJumpstart} alt='' />
+                        <img src={titleSmallDesignathon} alt='' />
+                        <p>April 14th ~ 16th, 2023</p>
+                    </div>
+                </div>
                 <div className='register-button-container'>
                     <div className='register-button register-button-blur'></div>
                     <a className='register-button' href={applicationLink} target="_blank">
                         <div className='register-button-text'>Register</div>
                     </a>
                 </div>
-                <div className='register-deadline'>Register Deadline: April 13th 11:59</div>
-                <div className='hero-description-container'>
-                    <div className='hero-description'>
-                        SB Creative Lab presents its 2023 JUMPSTART 
-                        Designathon! Get hands-on design experience by 
-                        challenging the normal with out-of-the-box ideas and 
-                        intentional design. Join us in kicking off our 3-day 
-                        challenge April 14th through April 16th. During this 
-                        time, students will participate on various design 
-                        workshops and meet new friends from other US college 
-                        campuses to tackle a design challenge together! 
-                    </div>
-                    <img src={descriptionDeco} className='hero-description-deco' alt='' />
-                </div>
+                <div className='register-deadline'>Register Deadline: April 13th 11:59 P.M.</div>
+                <p className='hero-description'>
+                    SB Creative Lab presents its 2023 JUMPSTART 
+                    Designathon! Get hands-on design experience by 
+                    challenging the normal with out-of-the-box ideas and 
+                    intentional design. Join us in kicking off our 3-day 
+                    challenge April 14th through April 16th. During this 
+                    time, students will participate on various design 
+                    workshops and meet new friends from other US college 
+                    campuses to tackle a design challenge together! 
+                </p>
             </div>
-            <img src={blob1} className='blob-1' alt='' />
+            <img src={blob1} className='blob-1' alt='' style={{ position: 'fixed', bottom: '-2px' }}/>
+            <footer style={{ fontSize: '12px', margin: '24px', textAlign: 'right' }}>more info coming soon...</footer>
         </div>
     )
 }
