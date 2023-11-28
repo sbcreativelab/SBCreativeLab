1b5ae376ab9a027e64d79bdb2808a7f4baa986ce
Ian Wen <h_wen@ucsb.edu>
2023-11-28 02:38:35 -0800

    updated PT page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react'
-import { Route } from "react-router-dom";
-import Home from './Components/Home'
-import Menu from './Components/Menu'
-import Oops from './Components/Oops'
-import About from './Components/About'
-import Resources from './Components/Resources'
-import Events from './Components/Events'
+import { Routes, Route } from "react-router-dom";
 import ReactGA from 'react-ga'
-import Challenge from './Components/Challenge';
-import Designathon from './Components/Designathon';
+
 import Projects from './Components/Projects';
-import W23Projects from './Components/W23Projects';
+import OldProjects from './Components/OldProjects';
 
 export class App extends Component {
   constructor(props) {
@@ -31,21 +24,11 @@ export class App extends Component {
   render() {
 
     return (
-      <div>
-        <head>
-          <title>SB Creative Lab</title>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></link>
-        </head>
-        <div className="main-body">
-          <Route exact path="/" render={(props) => <W23Projects {...props} />} />
-          <Route exact path="/f22" render={(props) => <Projects {...props} />} />
-          {/* <Route exact path="/challenge" render={(props) => <Challenge {...props}/>}/>
-              <Route exact path="/oops" render={(props) => <Oops {...props}/>}/>
-              <Route exact path="/about" render={(props) => <About {...props}/>}/>
-              <Route exact path="/resources" render={(props) => <Resources {...props}/>}/>
-              <Route exact path="/events" render={(props) => <Events {...props}/>}/>
-              <Route exact path="/designathon" render={(props) => <Designathon {...props}/>}/> */}
-        </div>
+      <div className="main-body">
+        <Routes>
+          <Route exact path="/" element={<Projects />} />
+          <Route exact path="/archived" element={<OldProjects />}  />
+        </Routes>
       </div>
     )
   }
