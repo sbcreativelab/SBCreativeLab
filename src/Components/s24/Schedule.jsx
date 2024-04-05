8c01ccf30fe9b1b55e180db49c19b6a6c28979d4
ru1nw <h_wen@ucsb.edu>
2024-04-05 19:17:26 +0000

    WIP working on schedule

--- src/Components/s24/Schedule.jsx
+++ src/Components/s24/Schedule.jsx
@@ -2,15 +2,28 @@ import React from 'react'
 import './Schedule.css'
 import useWindowDimensions from '../../utils/useWindowDimensions'
 
-import sectionTitle from '../../images/s24_images/schedule-title.svg'
 import sectionTitlePhone from '../../images/s24_images/phone_view/schedule-title-phone.svg'
+
+function ScheduleDay(props) {
+	const {date, events} = props
+	return <div className='schedule-table'>
+		<b>{ date }</b>
+		<table>
+			{ events.map((val, key) => {
+				return <tr key={ key }>
+					<td>{ val.time }</td>
+					<td>{ val.title }</td>
+					<td>{ val.desc }</td>
+				</tr>
+			}) }
+		</table>
+	</div>
+}
 
 export default function Schedule() {
     const { width } = useWindowDimensions();
     const PHONE_SCREEN_BREAKPOINT = 600;
     const isPhoneScreen = width <= PHONE_SCREEN_BREAKPOINT;
-	const gcalLink = "https://calendar.google.com/calendar/u/1?cid=ZjBiZmEyNzUxOWQ5MTNiNjQ3NzVjMDI0ZTc5MzA0MDNhYWE1ZjhhMTBiYWMwZjhlYTM5MDA5M2ExNTA1NGNkYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
-
 	return (
 		<div className='schedule' id='schedule'>
 			<div className='schedule-background' />
@@ -25,13 +38,141 @@ export default function Schedule() {
 					</div>
 					:
 					<div className='schedule-container'>
-						{ /* <img src={sideDeco} alt='' className='schedule-side-deco' /> */ }
-						<div className='schedule-content'>
-							<div className='schedule-header'>
-								<img src={sectionTitle} alt='Schedule' className='schedule-title' />
-							</div>
-						{ /* <img src={schedule} alt='' className='schedule-body' /> */ }
-						</div>
+						<h2>Schedule</h2>
+						<p>In-Person events, marked "IP" are open to UCSB/SBCC students only</p>
+						<ScheduleDay
+							date='Friday - Apr. 5, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'Team Formation Social (IP)',
+									desc: 'Join us @ UCSB Student Health Lawn'
+								},
+							]} />
+						<ScheduleDay
+							date='Monday - Apr. 8, 2024'
+							events={[
+								{
+									time: '5:00 - 5:30pm',
+									title: 'Workshop: Motion Design 101',
+									desc: <p>
+										Hosted by LottieFiles<br/>
+										Join us on Zoom
+									</p>
+								},
+								{
+									time: '5:30pm',
+									title: 'Team Formation Social',
+									desc: 'Join us on Zoom'
+								},
+							]} />
+						<ScheduleDay
+							date='Tuesday - Apr. 9, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'Workshop',
+									desc: <p>
+										Hosted by TBA<br/>
+										Join us on Zoom
+									</p>
+								},
+							]} />
+						<ScheduleDay
+							date='Wednesday - Apr. 10, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'Workshop: User Research',
+									desc: <p>
+										Hosted by Hannah Millar<br/>
+										Join us on Zoom
+									</p>
+								},
+							]} />
+						<ScheduleDay
+							date='Thursday - Apr. 11, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'Opening Ceremony',
+									desc: <p>
+										Join us on Zoom<br/>
+										Led by SBCL's Director: Tvisha Patel
+									</p>
+								},
+								{
+									time: '',
+									title: 'Keynote Speech',
+									desc: 'Led by Keynote Speaker: Felix Lee'
+								},
+								{
+									time: '',
+									title: 'Challenge Brief',
+									desc: 'Announcing this year\'s designathon theme and prompt'
+								},
+								{
+									time: '',
+									title: 'Work Session (IP)',
+									desc: <p>
+										Teams can work in person together 
+										at our work rooms. Free snacks and 
+										possible peer mentorship are 
+										provided.<br/>
+										Location: Library rooms ####, #### (TBA)
+									</p>
+								},
+							]} />
+						<ScheduleDay
+							date='Friday - Apr. 12, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'Workshop',
+									desc: <p>
+										Hosted by TBA<br/>
+										Join us on Zoom
+									</p>
+								},
+								{
+									time: '6:00pm',
+									title: 'Work Session (IP)',
+									desc: <p>
+										Teams can work in person together 
+										at our work rooms. Free snacks and 
+										possible peer mentorship are 
+										provided.<br/>
+										Location: Library rooms ####, #### (TBA)
+									</p>
+								},
+							]} />
+						<ScheduleDay
+							date='Wednesday - Apr. 17, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'Closing Ceremony',
+									desc: <p>
+										Join us on Zoom<br/>
+										Winners announced
+									</p>
+								},
+							]} />
+						<ScheduleDay
+							date='Friday - Apr. 19, 2024'
+							events={[
+								{
+									time: '6:00pm',
+									title: 'After Party (IP)',
+									desc: <p>
+										Come join us in a celebration for 
+										all of your amazing project 
+										submissions! Free snacks and drinks 
+										are provided.<br/>
+										Location: Student Resources Building
+									</p>
+								},
+							]} />
 					</div>
 				}
 		</div>
